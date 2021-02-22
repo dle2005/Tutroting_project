966159bcbf2d0801c1b05a9d561152480294634f
ujin0508@soongsil.ac.kr <ujin0508@soongsil.ac.kr>
2021-02-22 20:40:27 +0900

    메뉴랑 로그인

--- login.c
+++ login.c
@@ -1,13 +1,10 @@
 #include "head.h"
-void showLogin(void){
+void showMenU(void){
   
-    printf("-----로그인-----\n");
+    printf("\n-----로그인-----\n");
 
-    printf("아이디:");
-    scanf("%s",id);
-    printf("\n");
-    printf("비밀번호:");
-    scanf("%s",pw);
+    printf("       1.로그인\n");
+    printf("       2.회원가입\n");
 
 }
 
@@ -17,4 +14,20 @@ void checkID(){
 
 void checkPass(){
 
+}
+void loadDAta(info*parr){
+FILE * file =fopem("data.txt","wt");
+int i=0;
+for (i=0;i<3;i++)
+  {
+    printf("아이디:");
+    scanf("%s",id);
+    printf("\n");
+    printf("비밀번호:");
+    scanf("%s",pw);
+    fprintf(file, "%s %s",id,pw);
+      
+  }
+  flcose(fp);
+  return 0;
 }
