aa664c5e4a20fc9569bc75495e0f04c578c49d6d
ujin0508@soongsil.ac.kr <ujin0508@soongsil.ac.kr>
2021-02-22 20:53:08 +0900

    로그인메뉴

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #define PERSON 1000
 
-void showLogin(void); //로그인 창 출력
+void showMenu(void); //로그인 창 출력
 void loadData(info *,int*); //파일로부터 데이터 읽음
 
 
@@ -12,7 +12,7 @@ void loadData(info *,int*); //파일로부터 데이터 읽음
 void main() {
    info personal[PERSON]; //data저장
    int a=0;
-   showLogin();
+   showMenu();
    loadData(personal, &a);
 
 
