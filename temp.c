08f80aef618df28c037f248ec7d106cd81ca7310
YUuRiIM <yuri00805@naver.com>
2021-02-26 12:50:38 +0900

    0226

--- temp.c
+++ temp.c
@@ -1,30 +1,38 @@
- //이동
+//이동
 
-    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 
-    char a;
+    //char a;
 
     printf("앞으로 이동 <- ,  뒤로 이동 -> 을 누르세요");
 
-    scanf(% c, &a);
-    char c = getch();
-    char c = getche();
-    char c = getchar();
+//scanf(% c, &a);
+//char c = getch(); //사용
 
-    if a
-        == 75
-        {
+//char c = getche();
+//char c = getchar();
+int key;
 
-            /*fseek(fp,-10, SEEK_CUR);  //앞쪽으로 이동
 
-putchar(fgetc(fp));*/
-        }
+while (1)
+{
+    c = getch()
+    {
+        if a
+            == 75
+            {
 
-    if
-        else a == 77
-        {
+                fseek(fp, -20 * sizeof(word), SEEK_CUR); //앞쪽으로 이동
 
-            /*fseek(fp,10, SEEK_CUR);//뒤쪽으로 이동 
+                //putchar(fgetc(fp));
+            }
 
-putchar(fgetc(fp));*/
-        }
+        if
+            else a == 77
+            {
+
+                fseek(fp, , SEEK_CUR); //뒤쪽으로 이동
+
+                putchar(fgetc(fp));
+            }
+    }
--- wordbook.c
+++ wordbook.c
@@ -3,16 +3,49 @@
 void wordBook()
 {
     Word word;
+    int key;
 
     FILE *fp = fopen("Wordbook.txt", "r");
 
     printf("print word book\n");
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         fread(&word, sizeof(word), 1, fp);
 
         printf("%s : %s\n", word.eng_name, word.kor_name);
     }
 
-    fclose(fp);
+    while (1)
+    {
+        if (kbhit())
+        {
+
+            key = getch();
+            if (key == 224 || key == 0)
+            {
+                key = getch();
+                if (key == 75)
+                {
+                    fseek(fp, -20 * sizeof(word), SEEK_CUR);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        fread(&word, sizeof(word), 1, fp);
+                        printf("%s : %s\n", word.eng_name, word.kor_name);
+                    }
+                }
+
+                else if (key == 77)
+                {
+                    //fread(&word, sizeof(word), 1, fp);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        fread(&word, sizeof(word), 1, fp);
+                        printf("%s : %s\n", word.eng_name, word.kor_name);
+                    }
+                }
+            }
+        }
+        fclose(fp);
+    }
 }
