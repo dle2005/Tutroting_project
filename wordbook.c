61e7f8dce1189c1906373f68e9c2122bf68d52e8
YUuRiIM <yuri00805@naver.com>
2021-03-01 10:26:31 +0900

    commit

--- wordbook.c
+++ wordbook.c
@@ -4,7 +4,7 @@ void wordBook()
 {
     Word word;
     int key;
-
+    int page = 1;
     FILE *fp = fopen("Wordbook.txt", "r");
 
     printf("print word book\n");
@@ -25,7 +25,7 @@ void wordBook()
             if (key == 224 || key == 0)
             {
                 key = getch();
-                if (key == 75)
+                if (key == 75 && page>1) //<- 누를 떄 
                 {
                     fseek(fp, -20 * sizeof(word), SEEK_CUR);
                     for (int i = 0; i < 10; i++)
@@ -33,19 +33,34 @@ void wordBook()
                         fread(&word, sizeof(word), 1, fp);
                         printf("%s : %s\n", word.eng_name, word.kor_name);
                     }
+                    page--;
                 }
 
-                else if (key == 77)
+                else if (key == 77 && page<10) //-> 누를 때
                 {
-                    //fread(&word, sizeof(word), 1, fp);
+                    
                     for (int i = 0; i < 10; i++)
                     {
                         fread(&word, sizeof(word), 1, fp);
                         printf("%s : %s\n", word.eng_name, word.kor_name);
                     }
+                    page++;
                 }
+
+
+                else if (key == 27) //esc 누를 때
+                {
+                    break;
+                }
+
             }
         }
-        fclose(fp);
+        
     }
+    fclose(fp);
 }
+
+
+
+
+
