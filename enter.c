b896f7a3eaad16bd449973e8e85c2fe9d4bbfe24
DongKon Lee <dle0129@gmail.com>
2021-03-02 14:46:17 +0900

    fix bug

--- enter.c
+++ enter.c
@@ -5,41 +5,30 @@ void enter()
 {
     info user;
     info sign_user;
-    int i,j;
+    int i, j;
 
-     system("clear");
+    system("clear");
     printf("id:");
-    scanf("%s",&user.id);
+    scanf("%s", &user.id);
     printf("pw:");
-    scanf("%s",&user.pw);
-    
+    scanf("%s", &user.pw);
 
-    FILE *fp=fopen("UserInfo.txt","r+");
-    while(feof(fp)==0)
+    FILE *fp = fopen("UserInfo.txt", "ab+");
+    while (feof(fp) == 0)
     {
-        fread(&sign_user,sizeof(sign_user),1,fp);
-        if (strcmp(sign_user.id,user.id)==0)
+        fread(&sign_user, sizeof(sign_user), 1, fp);
+        if (strcmp(sign_user.id, user.id) == 0)
         {
             printf("exist id\n");
             Sleep(1000);
             system("clear");
 
             fclose(fp);
-            return enter();            
+            return enter();
         }
-        else if (strcmp(sign_user.id,user.id)!=0)
-        {
-            FILE *fp =fopen("UserInfo.txt","ab+");
-            fwrite(&user,sizeof(user),1,fp);
-            fclose(fp);
-            return ;
-        }
-        
+    }
 
-
-     }
-
-    
-
-
-}   
+    fwrite(&user, sizeof(user), 1, fp);
+    fclose(fp);
+    return;
+}
--- login.c
+++ login.c
@@ -1,30 +1,6 @@
 #include "head.h"
+
 void showMen(void){
-  
-    printf("\n-----로그인-----\n");
-
-    printf("       1.로그인\n");
-    printf("       2.회원가입\n");
-
-}
-
-
-
-void checkPass(){
-
-}
-void loadDAta(info*parr){
-FILE * file =fopen("data.txt","wt");
-int i=0;
-for (i=0;i<3;i++)
-  {
-    printf("아이디:");
-    scanf("%s",parr->id);
-    printf("\n");
-    printf("비밀번호:")
-    scanf("%s",parr->pw);
-    
-  }
-  fclose(file);
-  return 0;
+    printf("1. login\n");
+    printf("2. register\n");
 }
