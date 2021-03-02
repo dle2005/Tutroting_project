cc802c13664d8ff9f4e758b257ee845871e9c269
YUuRiIM <yuri00805@naver.com>
2021-03-03 02:27:38 +0900

    03.02

--- wordbook.c
+++ wordbook.c
@@ -13,8 +13,10 @@ void wordBook()
     {
         fread(&word, sizeof(word), 1, fp);
 
-        printf("%s : %s\n", word.eng_name, word.kor_name);
+        printf("%s : %s\n", word.eng_name, word.kor_name ,page);
+        
     }
+    printf("%d/10 page\n" , page);
 
     while (1)
     {
@@ -25,42 +27,49 @@ void wordBook()
             if (key == 224 || key == 0)
             {
                 key = getch();
-                if (key == 75 && page>1) //<- 누를 떄 
+                if (key == 75 && page > 1) //<- 누를 떄
                 {
                     fseek(fp, -20 * sizeof(word), SEEK_CUR);
                     for (int i = 0; i < 10; i++)
                     {
                         fread(&word, sizeof(word), 1, fp);
-                        printf("%s : %s\n", word.eng_name, word.kor_name);
+                        printf("%s : %s\n ", word.eng_name, word.kor_name);
+                        //printf("%d/10",page);
+                        // printf("%d/10", page);
+                        //printf("%d/10",page);
+                        // printf( )
+                        //페이지 숫자 알려주기, 처음페이지입니다, 마지막페이지입니다,
+                        //system("clear");
                     }
+                   // printf("%d/10\n",page);
                     page--;
-                }
-
-                else if (key == 77 && page<10) //-> 누를 때
+                    printf("%d/10 page\n", page);
+                    //system("clear");
+                    }
+                //system("clear");}
+                else if (key == 77 && page < 10) //-> 누를 때
                 {
-                    
+
                     for (int i = 0; i < 10; i++)
                     {
                         fread(&word, sizeof(word), 1, fp);
                         printf("%s : %s\n", word.eng_name, word.kor_name);
+                        
                     }
+                   // printf("%d/10\n",page);
                     page++;
+                    printf("%d/10 page\n", page);
+                    //system("clear");
                 }
-
-
+                //system("clear");
                 else if (key == 27) //esc 누를 때
                 {
+                    //printf("종료합니다.");
                     break;
                 }
-
             }
         }
-        
     }
-    fclose(fp);
+        fclose(fp);
+    
 }
-
-
-
-
-
