833ebad4b9ab31ccef7736259fa12c7725884a7c
ujin0508@soongsil.ac.kr <ujin0508@soongsil.ac.kr>
2021-03-03 14:01:19 +0900

    아이디 존재 기능

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include "head.h"
 
 void main() {
+     int i;
+    
+    while (1)
+    {
+        showMen();
+        scanf("%d", &i);
+        if (i == 1)
+            checkID();
+        else if (i == 2)
+            enter();
+    }
     quiz();
     wordBook();
     
 }
     
     
+
+
 
