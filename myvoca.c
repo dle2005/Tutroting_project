f54ff5b4159e57e58dbc643cafc11f14be675949
asdf1019 <78126526+asdf1019@users.noreply.github.com>
2021-03-14 23:34:17 +0900

    주석 추가

--- myvoca.c
+++ myvoca.c
@@ -1,6 +1,6 @@
 #include "head.h"
 
-void myvoca()
+void myvoca() //나만의 단어장
 {
     int m, n;
     char answer[10];
@@ -12,35 +12,35 @@ void myvoca()
     scanf("%d", &m);
     system("clear");
 
-    if (m == 1) //단어추가
+    if (m == 1) //나만의 단어장에 단어 추가하기
     {
-        FILE *fp = fopen("MyVoca.txt", "a+");
+        FILE *fp = fopen("MyVoca.txt", "a+"); //"MyVoca.txt" 파일 열기
         printf("English:");
-        scanf("%s", &word.eng_name);
+        scanf("%s", &word.eng_name); //영어단어 입력 받아서 word구조체 저장
         printf("Korean:");
-        scanf("%s", &word.kor_name);
-        fwrite(&word, sizeof(word), 1, fp);
+        scanf("%s", &word.kor_name); //한글단어 입력 받아서 word구조체 저장
+        fwrite(&word, sizeof(word), 1, fp); //"MyVoca.txt" 파일에 단어 저장
         fclose(fp);
         myvoca();
     }
-    else if (m == 2) //나만의단어장
+    else if (m == 2) //나만의 단어장 보기
     {   
         wordBook("MyVoca.txt");
         myvoca();
     }
-    else if (m == 3) //단어퀴즈
-    {   printf("1.multiple choice\n2.short-answer question");
+    else if (m == 3) //나만의 단어장 단어 퀴즈
+    {   printf("1.multiple choice\n2.short-answer question"); //1.객관식퀴즈 2.주관식퀴즈
         scanf("%d", &n);
         system("clear");
         if (n == 1) {
-            multipleQuiz("MyVoca.txt");
+            multipleQuiz("MyVoca.txt"); //"MyVoca.txt" 파일로 객관식 퀴즈
         } else if (n == 2) {
-            wordQuiz("MyVoca.txt");
+            wordQuiz("MyVoca.txt"); //"MyVoca.txt" 파일로 주관식 퀴즈
         }
         system("clear");
         myvoca();
     }
-    else if (m == 4)
+    else if (m == 4) //메뉴로 돌아가기
     {
         menu();
     }
--- wordquiz.c
+++ wordquiz.c
@@ -2,15 +2,17 @@
 
 void wordQuiz(char arr[])
 {
-    FILE *fp = fopen(arr, "rb");
-    FILE *wrongp = fopen("Wronganswer.txt", "w+");
+    FILE *fp = fopen(arr, "rb"); //매개변수 arr 파일 열기
+    FILE *wrongp = fopen("Wronganswer.txt", "w+"); //틀린 단어 저장할 "Wronganswer.txt"  파일 열기
 
     fseek(fp, 0, SEEK_END);
-    long file_size = ftell(fp) / sizeof(Word);
+    long file_size = ftell(fp) / sizeof(Word); //파일에 저장된 단어 수 읽기
     fseek(fp, 0, SEEK_SET);
 
     int rand_size = 10;
-    if(file_size < 10) rand_size = file_size;
+    if(file_size < 10) { //단어 수가 10개 미만일 때
+        rand_size = file_size; 
+    }
 
     Word word;
     Word wrongword[10] = {0,};
@@ -40,53 +42,53 @@ void wordQuiz(char arr[])
 
     //영한 한영 옵션
 
-    printf("1.eng-kor 2.kor-eng");
+    printf("1.eng-kor 2.kor-eng"); //1.영한퀴즈 2.한영퀴즈
     scanf("%d", &m);
 
-    if (m == 1) //영한
+    if (m == 1) //영한퀴즈
     {
         for (int i = 0; i < rand_size; i++)
         {
 
-            printf("process : %d%%\n", 100 / rand_size * i);
+            printf("process : %d%%\n", 100 / rand_size * i); //진행률 출력
             fseek(fp, sizeof(word) * random[i], SEEK_SET);
             fread(&word, sizeof(word), 1, fp);
-            printf("%s\n", word.eng_name);
-            scanf("%s", &answer);
+            printf("%s\n", word.eng_name); //영어단어 출력
+            scanf("%s", &answer); //답 입력받기
 
-            if (strcmp(answer, word.kor_name) == 0)
+            if (strcmp(answer, word.kor_name) == 0) //답과 한글단어 같을 때
             {
                 Beep(C, 50);
                 Beep(E, 50);
-                Beep(G, 50);
-                acc += 100 / rand_size;
+                Beep(G, 50); //딩동댕 소리
+                acc += 100 / rand_size; //정확도 증가
             }
             else
             {
-                Beep(HC, 150);
-                fwrite(&word, sizeof(word), 1, wrongp);
-                strcpy(wrongword[j].eng_name, word.eng_name);
-                strcpy(wrongword[j++].kor_name, word.kor_name);
+                Beep(HC, 150); //땡 소리
+                fwrite(&word, sizeof(word), 1, wrongp); //틀린단어 "Wronganswer.txt"  파일에 쓰기
+                strcpy(wrongword[j].eng_name, word.eng_name); //틀린단어 영어 복사
+                strcpy(wrongword[j++].kor_name, word.kor_name); //틀린단어 한글 복사
             }
             system("clear");
         }
         fclose(fp);
-        qsort(wrongword, 10, sizeof(word), compare1);
-        printf("accuracy : %d%%\n", acc);
+        qsort(wrongword, 10, sizeof(word), compare1); //틀린단어 정렬
+        printf("accuracy : %d%%\n", acc); //정확도 출력
         printf("wrong words : \n");
 
         fseek(wrongp, 0, SEEK_SET);
 
         for (int i = 0; i < j; i++)
         {
-            printf("%s %s\n", wrongword[i].eng_name, wrongword[i].kor_name);
+            printf("%s %s\n", wrongword[i].eng_name, wrongword[i].kor_name); //틀린단어 출력
         }
 
         printf("PRESS ESC\n");
 
         while (1)
         {
-            if ((ch = getch()) == 27)
+            if ((ch = getch()) == 27) //ESC키 누르면 나가기
             {
                 system("clear");
                 break;
@@ -101,10 +103,10 @@ void wordQuiz(char arr[])
             printf("process : %d%%\n", 100 / rand_size * i);
             fseek(fp, sizeof(word) * random[i], SEEK_SET);
             fread(&word, sizeof(word), 1, fp);
-            printf("%s\n", word.kor_name);
+            printf("%s\n", word.kor_name); //한글단어 출력
             scanf("%s", &answer);
 
-            if (strcmp(answer, word.eng_name) == 0)
+            if (strcmp(answer, word.eng_name) == 0) 
             {
                 Beep(C, 50);
                 Beep(E, 50);
@@ -121,7 +123,7 @@ void wordQuiz(char arr[])
             system("clear");
         }
         fclose(fp);
-        qsort(wrongword, 10, sizeof(word), compare2);
+        qsort(wrongword, 10, sizeof(word), compare2); 
         printf("accuracy : %d%%\n", acc);
         printf("wrong words : \n");
 
@@ -145,7 +147,7 @@ void wordQuiz(char arr[])
     }
 }
 
-int compare1(const void *a, const void *b)
+int compare1(const void *a, const void *b) //영어단어 정렬
 {
 
     char *c1, *c2;
@@ -155,7 +157,7 @@ int compare1(const void *a, const void *b)
     return strcmp(c1, c2);
 }
 
-int compare2(const void *a, const void *b)
+int compare2(const void *a, const void *b) //한글단어 정렬
 {
 
     char *c1, *c2;
